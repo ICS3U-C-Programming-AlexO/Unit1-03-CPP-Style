800346f9965de54e54d2d3af548209607e7cc7ba
Alexander O'Brien <amelia.obrien@stu.ocsb.ca>
2026-09-15 21:35:39 +0000

    First commit of helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2026 Alex OBrien All rights reserved.
+// .
+// Created by: Alex OBrien
+// Date: Sep 15th, 2026
+// This program displays a greeting with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Ms Raffin!" << std::endl;
+    std::cout << "Coding is cool!" << std::endl;
+}
